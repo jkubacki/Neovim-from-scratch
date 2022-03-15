7030fe09fcbde2067247da0aaa2f20cc67eedcd7
Jakub Kubacki <kubacki.jk@gmail.com>
2022-03-15 20:33:22 +0100

    Format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,13 +9,23 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		formatting.rubocop,
-		diagnostics.rubocop,
-		-- diagnostics.flake8
-	},
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    formatting.rubocop,
+    diagnostics.rubocop,
+    -- diagnostics.flake8
+  },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
+          augroup LspFormatting
+          autocmd! * <buffer>
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 10000)
+          augroup END
+          ]])
+    end
+  end,
 })
