4b6f45ff4bf34abfcd6cd55da98160165f746478
Jakub Kubacki <kubacki.jk@gmail.com>
2022-03-15 20:21:56 +0100

    Add rubocop formatting and diagnostics

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -85,6 +85,10 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
+  if client.name == "solargraph" then
+    client.resolved_capabilities.document_formatting = false
+  end
+
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
   end
